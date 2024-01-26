bde1f503f673c2f770d4f031f7e53b7945acf309
lisa56530 <145032722+lisa56530@users.noreply.github.com>
2024-01-26 15:47:32 +0100

    Create FonctionSupprimer.js

---
+++ src/main/resources/FonctionSupprimer.js
@@ -0,0 +1,9 @@
+let cible = document.getElementById("suppr") ;
+cible.addEventListener("click",action) ;
+ function action(event){
+ let ligneASuppr = element.parentNode.parentNode ; 
+ alert("clic sur le button : " + event.cible.id);
+ ligneASuppr.remove() ; 
+ }
+
+ 
